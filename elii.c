30728d330233021c4b44c32ab925728651fecdd9
Elina777 <elinathapa777@gmail.com>
2016-11-16 12:24:55 +0545

    Add files via upload

---
+++ elii.c
@@ -0,0 +1,21 @@
+/*
+student name :elina singh thapa
+subject:programming fundamental
+roll no:
+lab no:
+program:write a program to print divide of two numbers asked from users
+date:nov 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,divide;
+printf("enter first number");
+scanf("%f",&a);
+printf("enter second number");
+scanf("%f",&b);
+divide=a/b;
+printf("divide of two numbers is %f,divide");
+getch();
+return 0;
+}
