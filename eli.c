17235f1858859f7067c593db780c4627f0112dac
Elina777 <elinathapa777@gmail.com>
2016-11-16 11:04:00 +0545

    Add files via upload

---
+++ eli.c
@@ -0,0 +1,15 @@
+/*
+student name :elina singh thapa
+subject:programming fundamental
+roll no:
+lab no ;
+program:write a c program to print your name
+date:nov16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+printf("elina");
+getch();
+return 0;
+}
