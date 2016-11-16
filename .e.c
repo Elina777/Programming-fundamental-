3b89e5ea0da5e69747b3ab08fb15b0b6a841ef88
Elina777 <elinathapa777@gmail.com>
2016-11-16 10:55:29 +0545

    Add files via upload

---
+++ .e.c
@@ -0,0 +1,21 @@
+/*
+student name :elina singh thapa
+subject:programming fundamental
+roll no:
+lab no:01
+program :write a program to print product of two numbers asked from users
+date :nov16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,product;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter second number");
+scanf("%d",&b);
+product=a*b;
+printf("product of two numbers is %d",product);
+getch();
+return 0;
+}
