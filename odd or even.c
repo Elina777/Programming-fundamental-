54f729237b88d757c10203d6d6732aa2d10f1527
Elina777 <elinathapa777@gmail.com>
2016-11-30 12:57:52 +0545

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,23 @@
+/*
+student name: sushant aryal
+subject:programming fundamentals
+roll no:17
+lab sheet:06
+date:2016/10/25
+*/
+#include<stdio.h>
+int main()
+{
+   int a;
+printf("enter the number\n");
+scanf("%d",&a);
+        if(a%2==0)
+        {
+            printf("the number is even\n");
+        }
+    else
+   {
+        printf("the number is odd\n");
+   }
+    return 0;
+}
