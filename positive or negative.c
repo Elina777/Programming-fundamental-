245270248440a4627c043fc93d5ed1a2213e44c5
Elina777 <elinathapa777@gmail.com>
2016-11-30 12:55:56 +0545

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,23 @@
+/*
+student name: sushant aryal
+subject:programming fundamentals
+roll no:17
+lab sheet:06
+date:2016/10/25
+*/
+#include<stdio.h>
+int main()
+{
+   int a;
+printf("enter the number\n");
+scanf("%d",&a);
+        if(a>0)
+        {
+            printf("the number is positive\n");
+        }
+    else
+   {
+        printf("the number is negative\n");
+   }
+    return 0;
+}
